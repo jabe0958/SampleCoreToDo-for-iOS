012a03055f6557f9b55fee2f00d289736fe3e2ea
jabe0958 <jabe0958@gmail.com>
2017-09-13 10:30:44 +0900

    Initial Commit

---
+++ SampleCoreToDo for iOSTests/SampleCoreToDo_for_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  SampleCoreToDo_for_iOSTests.swift
+//  SampleCoreToDo for iOSTests
+//
+//  Created by jabe0958 on 2017/09/13.
+//  Copyright © 2017年 jabe0958. All rights reserved.
+//
+
+import XCTest
+@testable import SampleCoreToDo_for_iOS
+
+class SampleCoreToDo_for_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
