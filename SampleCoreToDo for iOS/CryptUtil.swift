534f6d807f884532ffba6017bd887be99824c54f
jabe0958 <jabe0958@gmail.com>
2017-09-17 15:13:12 +0900

    Comment is added for CryptUtil.

--- SampleCoreToDo for iOS/CryptUtil.swift
+++ SampleCoreToDo for iOS/CryptUtil.swift
@@ -12,10 +12,17 @@ import IDZSwiftCommonCrypto
 
 final public class CryptUtil {
     
+    // MARK: - Properties
+    
     static let log = OSLog(subsystem: "jp.tatsudoya.macos..SampleCoreToDo-for-iOS", category: "UTIL")
+    
+    // MARK: - Constructor
     
     private init() {}
     
+    // MARK: - Functions
+    
+    // 与えられた平文を与えられたキーフレーズでAES256CBCにて暗号化してBASE64形式の文字列で返します。
     static public func encryptAES256CBC(key: String, plainText: String) -> String {
         let keyHexString = utf8ToHexString(value: key)
         let keyHex = arrayFrom(hexString: keyHexString)
@@ -24,6 +31,7 @@ final public class CryptUtil {
         return dataFrom(byteArray: cipherData!).base64EncodedString()
     }
     
+    // 与えられたBASE64形式の文字列を与えられたキーフレーズでAES256CBCにて復号化して平文で返します。
     static public func decryptAES256CBC(key: String, cipherText: String) -> String {
         let keyHexString = utf8ToHexString(value: key)
         let keyHex = arrayFrom(hexString: keyHexString)
@@ -33,6 +41,7 @@ final public class CryptUtil {
         return decryptedText!.reduce("") { $0 + String(UnicodeScalar($1)) }
     }
     
+    // 与えられたUTF-8文字列を16進形式の文字列に変換して返します。
     static private func utf8ToHexString(value: String) -> String {
         return value.utf8.map { NSString(format: "%2X", $0) as String}.reduce("", {$0 + $1})
     }
